88d4b39dd2763add6e38837dc451a0468d875068
Adrian <adrianhakimutomo@gmail.com>
2024-03-30 19:55:50 +0700

    chores: add CORS

--- main.py
+++ main.py
@@ -1,9 +1,20 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 from typing import Union
 from ai_model import preprocess_text, generate_text_sampling_top_p_nucleus_22
 
 app = FastAPI()
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 class AIResponseModel(BaseModel):
     question: str
